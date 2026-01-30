c4ce1bcaf259794faf81490345c578606f10f3af
FrancescoAutera <>
2026-01-30 18:56:51 +0100

    setup projects

---
+++ Assets/Prefabs/TestPluginfs.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+namespace maskGameJam
+{
+    
+    
+public class TestPluginfs : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
