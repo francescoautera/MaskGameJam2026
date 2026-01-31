a8a14695aad13f4f4ef93ab3e4814f9167eb1f01
FrancescoAutera <>
2026-01-31 08:38:23 +0100

    add dialogue baloon

--- Assets/Script/ScriptableObjects/DialogueData.cs
+++ Assets/Script/ScriptableObjects/DialogueData.cs
@@ -5,7 +5,7 @@ public class DialogueData : ScriptableObject
 {
     [SerializeField] private Sprite _portrait;
     [SerializeField] private string _name;
-    [SerializeField] private string _line;
+    [SerializeField,TextArea(3,3)] private string _line;
 
     public Sprite Portrait => _portrait;
     public string Name => _name;
