5b3692ca77960b086bf3a518e4f610a4bc12a430
Gabriele Coppola <gabocop999@gmail.com>
2026-01-31 16:36:27 +0100

    add vignette pulse fullscreen shader

---
+++ Assets/Script/Feedbacks/FullScreen.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using EasyButtons;
+using UnityEngine;
+
+namespace GameJam
+{
+    public class FullScreenEffectController : MonoBehaviour
+    {
+        private static readonly int CutLevel = Shader.PropertyToID("_CutLevel");
+        [SerializeField] private Material _material;
+        [SerializeField] private float _animationTime = .2f;
+        [SerializeField] private AnimationCurve _animationCurve;
+
+
+        private float _currentCutLevel;
+
+        private void Start()
+        {
+            _currentCutLevel = _material.GetFloat(CutLevel);
+        }
+
+        [Button]
+        public void SetCutLevel(float level)
+        {
+            StartCoroutine(SmoothFloatChangeCoroutine(_currentCutLevel, level, CutLevel, _animationTime));
+        }
+
+        
+        
+        private IEnumerator SmoothFloatChangeCoroutine(float startValue, float endValue, int propertyNameID,
+            float animationTime)
+        {
+            float t = 0;
+
+            while (t <= animationTime)
+            {
+                var currentValue = Mathf.Lerp(startValue, endValue, _animationCurve.Evaluate(t / animationTime));
+                _material.SetFloat(propertyNameID, currentValue);
+                t += Time.deltaTime;
+                yield return null;
+            }
+
+            _material.SetFloat(propertyNameID, endValue);
+            _currentCutLevel = endValue;
+        }
+    }
+}
